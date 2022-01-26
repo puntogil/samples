ebbc0cddd2bf4bf23b2725c1bea9d341856a4c25
puntogil <puntogil@libero.it>
2022-01-26 11:14:44 +0100

    init

---
+++ es_Array/Array/src/it/tss/es_array/Array.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it.tss.es_array;
+
+import java.util.Random;
+
+/**
+ *
+ * @author tss
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //int [] input = {12,34,56,3,57,3,5,6,67,45};
+        int[] input = generaArrayCasuale(50, 1000);
+        ArrayTool.stampa(input);
+        ArrayTool.max(input);
+        ArrayTool.min(input);
+        ArrayTool.media(input);
+        ArrayTool.ordina(input);
+    }
+
+    /**
+     *
+     * @param ncelle Numero celle dell'array
+     * @param max massimo valore dell'intero contenuto nell'array
+     * @return un'array con un numero celle pradefinito, e contenente valori interi
+     */
+    private static int[] generaArrayCasuale(int ncelle, int max) {
+        int[] ris = new int[ncelle];
+        for (int i = 0; i < ris.length; i++) {
+            ris[i] = generaNumeroCasuale(max);
+        }
+        return ris;
+    }
+
+    /**
+     *
+     * @param max numero massimo di numeri interi nell'array
+     * @return Valore di un numero intero che verra assegnato all'array
+     */
+    private static int generaNumeroCasuale(int max) {
+        int min_val = 0;
+        Random ran = new Random();
+        return ran.nextInt(max) + min_val;
+    }
+}
---
+++ es_Array/Array/src/it/tss/es_array/ArrayTool.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.tss.es_array;
+
+/**
+ *
+ * @author tss
+ */
+public class ArrayTool {
+
+    static void stampa(int[] numeri) {
+        for (int i = 0; i < numeri.length -1; i++) {
+            System.out.print(numeri[i] + ", ");
+        }
+        System.out.println("");
+    }
+    
+    static void stampa(int numero) {
+        System.out.println("trovato " + numero);
+    }
+    
+    static boolean ricerca(int numeri[], int numero) {
+        boolean trovato = false;
+        for (int i = 0; i < numeri.length -1; i++) {
+            if ( numeri[i] == numero){
+                trovato = true;
+            }
+        }
+        if (trovato == true) stampa(numero);
+        return trovato;
+    }
+
+    static int max(int numeri[]) {
+        int massimo = 0;
+        for (int i = 0; i < numeri.length -1; i++) {
+            if (numeri[i] > massimo )
+                massimo  = numeri[i];
+        }
+        stampa(massimo);
+        return massimo;
+    }
+
+    static int min(int numeri[]) {
+        int minimo = 0;
+        for (int i = 0; i < numeri.length -1; i++) {
+            if (numeri[i] < minimo )
+                minimo  = numeri[i];
+        }
+        stampa(minimo);
+        return minimo;
+    }
+
+    static int media(int numeri[]) {
+        int somma = 0;
+        for (int i = 0; i < numeri.length -1; i++) {
+                somma += numeri[i];
+        }
+        somma = somma/numeri.length;
+        stampa(somma);
+        return somma;
+    }
+
+    static void ordina(int numeri[]) {
+        int temp = 0;
+        for (int i = 0; i < numeri.length; i++) {
+            for (int j = 0; j < numeri.length; j++) {
+                if (numeri[i] > numeri[j]){
+                    temp = numeri[i];
+                    numeri[i] = numeri[j];
+                    numeri[j] = temp;
+                }
+            }  
+        }
+        stampa(numeri);
+    }
+}
